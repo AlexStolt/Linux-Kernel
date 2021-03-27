4fa3eafbefd572ad44f356f672bf3e6666df3001
AlexStolt <stalexandros@uth.gr>
2021-03-27 10:06:43 +0200

    SYSFS

--- Homeworks/HW_00/System Call/User Level/main.c
+++
@@ -1,6 +0,0 @@
-#include "syscall_wrapper.h"
-
-int main(){
-	find_roots_syscall_wrapper();
-	return 0;
-}
--- Homeworks/HW_00/System Call/User Level/roots.c
+++ Homeworks/HW_00/System Call/User Level/roots.c
@@ -1,7 +1,7 @@
 #include <sys/syscall.h>
 #include <unistd.h>
-#include "syscall_wrapper.h"
+#include "roots.h"
 
 int find_roots_syscall_wrapper(void){
-	return (syscall(__NR_find_roots));
+    return (syscall(__NR_find_roots));
 }
